a7104f48addfd18b80ac4ed1bcc986af4f503a7a
edwardmccormick <66527737+edwardmccormick@users.noreply.github.com>
2021-02-25 09:19:02 -0600

    Updated script and finished exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,11 +2,11 @@ USE codeup_test_db;
 
 CREATE table albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(50) DEFAULT 'Unnamed',
+    artist VARCHAR(50) NOT NULL,
     name VARCHAR(100) DEFAULT 'Untitled',
     release_date INT(4),
-    sales FLOAT(10,4) DEFAULT 'Unknown',
+    sales FLOAT,
     genre VARCHAR(20) DEFAULT 'Unknown',
-
-)
+    PRIMARY KEY (id)
+);
 
